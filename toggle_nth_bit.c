a2a26d8301096764a3e93293891dff97a6592dc0
GudiyaNaveen <98573943+GudiyaNaveen@users.noreply.github.com>
2022-02-14 14:10:33 +0530

    Program to toggle nth bit of a number.

---
+++ toggle_nth_bit.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void toggle_nth_bit(int ,int );
+int main()
+{
+	int x,n;
+  printf(""Enter a number: );
+  scanf("%d",&x);
+	printf("Enter nth bit\n");
+	scanf("%d",&n);
+	toggle_nth_bit(x,n);
+	return 0;
+}
+
+void toggle_nth_bit(int x,int n)
+{
+  x = x ^ (0x1<<n);
+	printf("value of x=%d\n",x);
+}
