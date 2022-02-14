07aff76b6f56730e5bf1e4b7484100d772735d1e
GudiyaNaveen <98573943+GudiyaNaveen@users.noreply.github.com>
2022-02-14 14:32:37 +0530

    program to copy one string to another string and find length of new string using pointers.

---
+++ strcpy_len_pointer.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+void Mystrcpy(char *,char *);
+int main()
+{
+    char str1[1000],str2[1000];
+    printf("Enter a string:");
+    scanf("%[^\n]s",str1);
+    Mystrcpy(str2,str1);
+    printf("string copy str2<--str1: %s\n",str2);
+    return 0;
+}
+
+void Mystrcpy(char *s2,char *s1)
+{
+    int len=0;
+    while((*s2++=*s1++)!='\0')
+    {
+      len++;
+    }
+    printf("length of string: %d\n",len);
+}
