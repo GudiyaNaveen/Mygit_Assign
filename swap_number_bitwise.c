9c04464c983ac602ddf817d7227e97f2d9bc6370
GudiyaNaveen <98573943+GudiyaNaveen@users.noreply.github.com>
2022-02-14 14:14:12 +0530

    Program to swap two numbers using bitwise operator.

---
+++ swap_number_bitwise.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void swap_two_num(int ,int );
+int main()
+{
+	int x,y;
+  printf("Enter two numbers: ");
+  scanf("%d%d",&x,&y);
+	printf("Before Swap : %d %d\n",x,y);
+	swap_two_num(x,y);
+	return 0;
+}
+
+void swap_two_num(int x,int y)
+{
+  x=x^y;
+	y=x^y;
+	x=x^y;
+	printf("After Swap : %d %d\n",x,y);
+}
