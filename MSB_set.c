1fbd52a4712c213a44f3cb5f5846da91409c0edf
GudiyaNaveen <98573943+GudiyaNaveen@users.noreply.github.com>
2022-02-14 13:54:43 +0530

    Program to check Most Significant Bit (MSB) of a number is set or not.

---
+++ MSB_set.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+  printf("Enter a number: ");
+  scanf("%d",&n);
+	if(n & 0x80000000)
+  {
+		printf("MSB bit is set\n");
+  }
+	else
+  {
+		printf("MSB bit is not set\n");
+  }
+	return 0;
+}
